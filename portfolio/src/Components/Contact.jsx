ebfa8317eb446cd750d46ca4a52b4b8d160edba6
Stephen <steve_cocks28@hotmail.com>
2022-05-16 10:00:38 +0100

    contact section changed

--- portfolio/src/Components/Contact.jsx
+++ portfolio/src/Components/Contact.jsx
@@ -1,33 +1,25 @@
 import "../Styles/Contact.css";
 import logo from "../Images/linkedin.png"
-import ContactForm from "./ContactFrom";
 
 export default function Contact() {
 	return (
 		<div class="contact-section">
 			<h1>Contact</h1>
-		<div class="contact">
-			<div className="contact-form-message">
-			Feel free to email me directly and ask any questions!
-			</div>
-			<div className="contact-form">
-				<ContactForm />
-			</div>
-			<div class="contact-details"></div>
-			<div className="contact-me">You can take my details and reach out to me on any of the below!</div>
+			<div class="contact-details">
+			<div className="contact-me">Please reach out to me on any of the below!</div>
 			<br />
 			<div className="number">
-				<h2>Phone number</h2>
-				<p>078737 14915</p>
+				<h2>Phone Number</h2>
+				<p>07873 714 915</p>
 			</div>
 			<br />
 			<div className="email">
-				<h3>Email Address</h3>
+				<h2>Email Address</h2>
 				<p>steve_cocks28@hotmail.com</p>
 			</div>
 			<br />
 			<div className="linkedin">
-				<h3>LinkedIn</h3>
+				<h2>LinkedIn</h2>
 				<a
 					href="https://www.linkedin.com/in/stephen-cocks-1641a4238/"
 					target="_blank"
