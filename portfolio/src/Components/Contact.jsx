ab6e55ebccdc4eff30b7de2f025c39d5902693ea
Stephen <steve_cocks28@hotmail.com>
2022-05-16 10:18:19 +0100

    small changes

--- portfolio/src/Components/Contact.jsx
+++ portfolio/src/Components/Contact.jsx
@@ -1,16 +1,25 @@
 import "../Styles/Contact.css";
-import logo from "../Images/linkedin.png"
+import logo from "../Images/linkedin.png";
+import glogo from "../Images/github-logo.png";
+import elogo from "../Images/email.jpeg";
 
 export default function Contact() {
 	return (
 		<div class="contact-section">
+			<div class="contact-list">
 			<h1>Contact</h1>
 			<div class="contact-details">
 			<div className="contact-me">Please reach out to me on any of the below!</div>
 			<br />
-			<div className="number">
-				<h2>Phone Number</h2>
-				<p>07873 714 915</p>
+			<div className="github">
+				<h2>Github</h2>
+				<a
+					href="https://github.com/Stephen2501"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<img src={glogo} alt=" " height="50vw" width="50vw" />
+				</a>
 			</div>
 			<br />
 			<div className="email">
@@ -28,6 +37,7 @@ export default function Contact() {
 					<img src={logo} alt=" " height="50vw" width="50vw" />
 				</a>
 			</div>
+		</div>
 		</div>
 		</div>
 	);
